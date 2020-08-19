72435141220e71c4a9aa5dc5b4488a23935a3e46
xiaows <jian958753831@qq.com>
2020-08-19 10:28:44 +0800

    add

---
+++ README.md
@@ -0,0 +1,14 @@
+# 占位图
+
+## install
+
+```
+const serve = require('dummy-image')
+serve()
+```
+
+## Usage
+
+```
+/widthxheight/bgColor/color?text="Hello, Tieba"
+```
--- src/index.js
+++ src/index.js
@@ -1,53 +1,61 @@
-const koa = require('koa')
-const address = require('address')
-const createImageStream = require('./createImageStream')
+const koa = require('koa');
+const address = require('address');
+const createImageStream = require('./createImageStream');
 
 const defaults = {
-    port: 8080,
-}
+    port: 8080
+};
 
-const ip = address.ip()
-const app = new koa()
+const ip = address.ip();
+const app = new koa();
 
-const re = /^\/(\d+(\.\d+)?x\d+(\.\d+)?)(\/([A-Fa-f0-9]{6}))?(\/([A-Fa-f0-9]{6}))?/
+const re = /^\/(\d+(\.\d+)?x\d+(\.\d+)?)(\/([A-Fa-f0-9]{6}))?(\/([A-Fa-f0-9]{6}))?/;
 
-module.exports = function (config) {
-    const port = config.port || process.env.PORT || defaults.port
+module.exports = function (config = {}) {
+    const port = config.port || process.env.PORT || defaults.port;
 
     app.use(async function (ctx, next) {
-        const reqpath = ctx.path
-        let ret
+        const reqpath = ctx.path;
+        let ret;
 
         if ((ret = re.exec(reqpath))) {
-            const wh = ret[1]
-            const bgColor = ret[5]
-            const color = ret[7]
-            const text = ctx.query['text']
+            const wh = ret[1];
+            const bgColor = ret[5];
+            const color = ret[7];
+            const text = ctx.query['text'];
 
-            const [width, height] = wh.split('x')
+            const [width, height] = wh.split('x');
 
             const stream = createImageStream({
                 width: +width,
                 height: +height,
                 bgColor,
                 color,
-                text,
-            })
+                text
+            });
 
-            ctx.set('Content-Type', 'image/png')
+            ctx.set('Content-Type', 'image/png');
 
-            ctx.body = stream
+            ctx.body = stream;
         } else {
-            await next()
+            await next();
         }
-    })
+    });
+
+    app.use(async function (ctx, next) {
+        if (ctx.path === '/') {
+            ctx.body = 'Dummy Image Run...';
+        } else {
+            return await next();
+        }
+    });
 
     app.listen(port, function (err) {
         if (err) {
-            console.error(err)
-            process.exit(1)
+            console.error(err);
+            process.exit(1);
         } else {
-            console.log(`✨ Server on: http://${ip}:${port}`)
+            console.log(`✨ Server on: http://${ip}:${port}`);
         }
-    })
-}
+    });
+};
