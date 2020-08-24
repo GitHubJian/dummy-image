6681c0f95c14902a77a6d923890e39e0191386e8
xiaows <jian958753831@qq.com>
2020-08-24 10:54:41 +0800

    add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,17 @@ const pkg = require('./package.json');
 const {name: appname} = pkg;
 
 module.exports = {
-    name: appname,
-    script: 'npm start',
-    instance: 1,
-    cwd: '.',
-    error_file: `./logs/${appname}-err.log`,
-    out_file: `./logs/${appname}-out.log`,
-    env: {
-        NODE_ENV: 'production'
-    }
+    apps: [
+        {
+            name: appname,
+            script: 'npm start',
+            instance: 1,
+            cwd: '.',
+            error_file: `./logs/${appname}-err.log`,
+            out_file: `./logs/${appname}-out.log`,
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+    ]
 };
